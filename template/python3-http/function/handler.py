ff8ea0b9c8a836edd808a6e2b2dd8eacaf7ef47c
Vincenzo Chianese <vincenz.chianese@icloud.com>
2021-09-10 11:18:34 -0500

    add fake handler

--- template/python3-http/function/handler.py
+++ template/python3-http/function/handler.py
@@ -1,2 +1,5 @@
 def handle(event, context):
     return {"statusCode": 200, "body": "Hello from OpenFaaS!"}
+
+def action(event, context):
+    return {"statusCode": 200, "body": "Hello from Action!"}
